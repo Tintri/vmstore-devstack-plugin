4675094bd92856046adb9d108b4d4fa7063100c5
Alexey Khodos <akhodos@tintri.com>
2025-03-28 14:31:44 -0700

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     rm -rf vmstore-cinder-driver
-    git clone https://github.com/Tintri/vmstore-cinder-driver.git
+    git clone git@github.com:Tintri/vmstore-cinder-driver.git
     mkdir -p /opt/stack/cinder/cinder/volume/drivers/vmstore
     cp -rf vmstore-cinder-driver/* /opt/stack/cinder/cinder/volume/drivers/vmstore/
 fi
